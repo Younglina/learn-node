491e6cd3ef1c4d0bc556e959813dc9337d2e43b1
汪志强 <younglina0409@outlook.com>
2023-04-21 10:13:02 +0800

    feat: koa-body参数解析 upload

--- koa/index.js
+++ koa/index.js
@@ -1,27 +1,10 @@
 const Koa = require('koa')
 const userRouter = require('./router')
-const path = require('path');
 const { koaBody } = require('koa-body')
 
 const app = new Koa()
 
-app.use(koaBody({
-  multipart: true,
-  formidable: {
-    uploadDir: path.join(__dirname,'./uploads'),
-    // 保持后缀名\
-    keepExtensions: true,
-    // 文件大小
-    maxFileSize: 2 * 1024 * 1024, // 设置上传文件大小限制，默认2M
-    onFileBegin: (name, file) => {
-      //修改上传文件名
-      file.filepath = path.join(__dirname, "./uploads/") + Date.now() + file.originalFilename;
-    },
-    onError(err){
-        console.log(err)
-    }
-  }
-}));
+app.use(koaBody());
 app.use(userRouter.routes())
 app.use(userRouter.allowedMethods())
 
--- koa/router.js
+++ koa/router.js
@@ -1,20 +1,33 @@
 const Router = require('koa-router')
-const router = new Router({prefix: '/user'})
+const router = new Router({ prefix: '/user' })
+const { koaBody } = require('koa-body')
+const path = require('path');
 
-router.get('/', (ctx, next)=>{
+router.get('/', (ctx, next) => {
   console.log(ctx.query)
   ctx.body = "get"
 })
-// router.get('/:id', (ctx, next)=>{
-//   ctx.body = "get"
-//   console.log(ctx.query)
-//   console.log(ctx.params)
-// })
-router.post('/', (ctx,  next)=>{
+router.post('/', (ctx, next) => {
   console.log(ctx.request.body)
   ctx.body = "post"
 })
-router.post('/upload', (ctx, next)=>{
+router.post('/upload', koaBody({
+  multipart: true,
+  formidable: {
+    uploadDir: path.join(__dirname, './uploads'),
+    // 保持后缀名\
+    keepExtensions: true,
+    // 文件大小
+    maxFileSize: 2 * 1024 * 1024, // 设置上传文件大小限制，默认2M
+    onFileBegin: (name, file) => {
+      //修改上传文件名
+      file.filepath = path.join(__dirname, "./uploads/") + Date.now() + file.originalFilename;
+    },
+    onError(err) {
+      console.log(err)
+    }
+  }
+}), (ctx, next) => {
   console.log(ctx.request.body)
   ctx.body = "upload"
 })
