06da091d9285bf3e8c990a13c450e81cc3d5fb5e
younglina <younglina0409@outlook.com>
2023-05-05 23:06:39 +0800

    chore: 备注

--- src/middleware/userVerify.m.js
+++ src/middleware/userVerify.m.js
@@ -3,6 +3,7 @@ const { query } = require('../utils/cloud')
 const crypto = require('crypto');
 const { CRYPTO_ITER, CRYPTO_KEYLEN, CRYPTO_HASH } = require('../app/config')
 
+// 名称和密码的必填校验
 const namePassRequired = async (ctx, next) => {
   const { name, password } = ctx.request.body
   if (!name || !password) {
@@ -11,19 +12,19 @@ const namePassRequired = async (ctx, next) => {
   await next()
 }
 
+// 密码加密
 const passwordEncrypt = async (ctx, next) => {
   const { password } = ctx.request.body
 
-  // 存储密码
+  // 获取盐值和加密后的密码
   const { salt, hashPassword } = getHashPassword(password);
-  // 将salt和hash存储在数据库中
-
+  // 传递盐值和加密后的密码
   ctx.request.body.password = hashPassword
   ctx.request.body.salt = salt
   await next()
 }
 
-// 验证密码是否正确
+// 验证密码是否正确，验证通过返回用户信息
 const userVerify = async (ctx, next) => {
   const { name, password } = ctx.request.body
   const res = await query('node_user', {name}, ['salt', 'password', 'name'])
@@ -45,8 +46,8 @@ const userVerify = async (ctx, next) => {
 function getHashPassword(password) {
   // 生成随机盐值
   const salt = crypto.randomBytes(16).toString('hex');
-  // password 被加密值 , salt 盐值, iterations 迭代次数, keylen 生成的密钥长度, 'sha512' 哈希算法
-  const hashPassword = crypto.pbkdf2Sync(password, salt, 10, 32, 'sha512').toString('hex');
+  // password 被加密值 , salt 盐值, iterations 迭代次数, keylen 生成的密钥长度, 哈希算法
+  const hashPassword = crypto.pbkdf2Sync(password, salt, CRYPTO_ITER, CRYPTO_KEYLEN, CRYPTO_HASH).toString('hex');
   return { salt, hashPassword };
 }
 
